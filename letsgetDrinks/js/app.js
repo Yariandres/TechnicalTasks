c07d193fe73e814ec199eef2582350ef9923a35f
Yari Andres <yari.andres@gmail.com>
2019-12-03 20:17:47 +0100

    final project

--- letsgetDrinks/js/app.js
+++ letsgetDrinks/js/app.js
@@ -11,66 +11,62 @@ window.onload = async () => {
 };
 
 createList = eventList => {
-  document.querySelector('#event-list').innerHTML = eventList.map( event => 
-    `<div class="card"> 
-        <div class="card__event--type">
-          <p><strong>Type: </strong>${event.type}</p>           
-        </div>
-
-        <div class="card__time">
-          <p><strong>time: </strong>${event.time}</p>           
-        </div>
-
-        <div class="card__title">
-          <p><strong>title: </strong>${event.title}</p>           
-        </div>
-
-        <div class="card__location">        
-          <p><strong>Location: </strong>${event.location.name}</p>
-        </div>
-
-        <div class="card__location--cordinates">
-          <p><strong>location:</strong></p>
-          <iframe
-            width="600"
-            height="450"
-            frameborder="0" style="border:0"
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBLP6dT7PuK2uoIKXnETfnl5mOP_vjC4YY&q=${event.location.latitude}+${event.location.longitude}">
-          </iframe>
-          
-                  
-        </div>
-
-        <div class="card__organiser">
-          <p><strong>Creator: </strong>${event.creator.name}</p>
-        </div>
+  document.querySelector('#event-list').innerHTML = eventList.map( event =>    
+    `<div class="col-xs-2 col-sm-4 col-md-4">
+      <div class="card-wrap mt-5">
+            
+            <h2 class="card-wrap_title text-center my-5">${event.type}</h2>
         
-        <div class="card__avatarUrl">
-          <img src="${event.creator.avatarUrl}">
-        </div>
+        <p class="card-text"><strong>time: </strong>${event.time}</p>       
+        <p class="card-text"><strong>title: </strong>${event.title}</p>            
+        <p class="card-text"><strong>Location: </strong>${event.location.name}</p>
+        <p class="card-text"><strong>location:</strong></p>
+        <iframe
+        class="mb-2"
+          width="100%"
+          height="100%"
+          frameborder="0" style="border:0"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBLP6dT7PuK2uoIKXnETfnl5mOP_vjC4YY&q=${event.location.latitude}+${event.location.longitude}">
+        </iframe>
+        
+          <h2 class="mt-4">Organiser: </h2>
+                   
+            <img class="card-wrap__creator mt-4" src="${event.creator.avatarUrl}">
+          
 
-        <div class="card__guests">
-          <p>Guests:</p>
+          
+            <h2 class="card-wrap__creatortext mt-4 ml-3">${event.creator.name}</h2>
+    
+         
+
+        <div class="card-body">
+          <h3 class="mb-4">Guests:</h3>
           ${event.guests.map(guests => 
-            `<p>${guests.name}</p>
-              <img width="50" height="50" src="${guests.avatarUrl}">
+            `<ul class="list-unstyled">
+              <li><img class="card-wrap__guest mb-4" width="50" height="50" src="${guests.avatarUrl}"><span class="ml-4">${guests.name}</span></li>
+            </ul>
+              
             `
             ).join("")
           }
-        </div>
-        <div class="card__comments">
-          <strong>Comments: </strong>
-            ${event.comments.map(comment => 
-              `<p>${comment.user.name}</p>
-                <img width="20" height="20" src="${comment.user.avatarUrl}">
-                <span>${comment.message}</span>
-                <span>${comment.timestamp}</span>
-              `
-            ).join("")
-          }
-        </div>
-        <hr>
-      </div>`
+          <div class="card-body">
+            <h2><strong>Comments: </strong></h2> 
+              ${event.comments.map(comment => 
+                `<p>${comment.user.name}</p>
+                  <img class="card-wrap__avatar" width="20" height="20" src="${comment.user.avatarUrl}">
+                  <span>${comment.message}</span>
+                  <br>
+                  <p class="mt-2">${comment.timestamp}</p>
+                  <hr>
+                `
+              ).join("")
+            }
+          </div>    
+        </div>  
+      </div>
+    </div>    
+    <hr>
+    `
   ).join('');
 }
 
